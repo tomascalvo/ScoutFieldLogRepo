efb2558b8a41953a1b8c08e589cf8a36b535b61a
Jake <jworsham9697@gmail.com>
2020-06-13 14:57:57 -0400

    w/ Chris created the ScoutForm View and related action in HomeController.

---
+++ ScoutFieldLog_GroupProject/Models/BackupDbContext.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata;
+//using Microsoft.Extensions.Configuration;
+
+//namespace ScoutFieldLog_GroupProject.Models
+//{
+//    public partial class ScoutFieldLogDbContext : DbContext
+//    {
+//        //This was added
+//        public IConfiguration Configuration { get; }
+//        public ScoutFieldLogDbContext()
+//        {
+//        }
+
+//        public ScoutFieldLogDbContext(DbContextOptions<ScoutFieldLogDbContext> options)
+//            : base(options)
+//        {
+//        }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//            //this was added
+//                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+//            }
+//        }
+
+//        protected override void OnModelCreating(ModelBuilder modelBuilder)
+//        {
+//            OnModelCreatingPartial(modelBuilder);
+//        }
+
+//        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+//    }
+//}
