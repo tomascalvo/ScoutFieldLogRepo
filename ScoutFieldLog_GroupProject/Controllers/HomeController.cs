0fcda7b7c05d0dc8b3e39918c6164dd57265dd28
chris <caconstan@yahoo.com>
2020-06-13 19:34:44 -0400

    added recaptcha to scout form

--- ScoutFieldLog_GroupProject/Controllers/HomeController.cs
+++ ScoutFieldLog_GroupProject/Controllers/HomeController.cs
@@ -1,17 +1,21 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ScoutFieldLog_GroupProject.Models;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
 
 namespace ScoutFieldLog_GroupProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ScoutFieldLogDbContext _context;
+        private SeamlessDAL DAL;
 
-        public HomeController(ScoutFieldLogDbContext context)
+        public HomeController(IConfiguration iconfig, ScoutFieldLogDbContext context)
 
         {
             _context = context;
+            DAL = new SeamlessDAL(iconfig);
         }
 
         public IActionResult Index()
@@ -31,14 +35,21 @@ namespace ScoutFieldLog_GroupProject.Controllers
         }
 
         [HttpPost]
-        public IActionResult ScoutForm(StartUp startup)
+        public async Task<IActionResult> ScoutForm(StartUp startup, string token)
         {
-            _context.Add(startup);
-            _context.SaveChanges();
-            ViewBag.message = "Thank you for submitting your startup company!";
+            //ViewBag.message = DAL.Recaptcha(startup.Status);
+            if (await DAL.Recaptcha(token)) {
+                startup.Status = "";//clear the token so we don't save to DB
+                _context.Add(startup);
+                _context.SaveChanges();
+                ViewBag.message = "Thank you for submitting your startup company!";
+            } else
+            {
+                ViewBag.message = "Token not valid";
+            }
             return View();
         }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
