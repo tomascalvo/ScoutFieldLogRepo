2f9930283cc167a0919f02f114d4d60189c734be
andres-torres-2020 <andres.torres.192020@gmail.com>
2020-06-13 19:05:41 -0400

    Added Index page functionality: login, logout, search, search results

--- ScoutFieldLog_GroupProject/Controllers/HomeController.cs
+++ ScoutFieldLog_GroupProject/Controllers/HomeController.cs
@@ -1,22 +1,51 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ScoutFieldLog_GroupProject.Data;
 using ScoutFieldLog_GroupProject.Models;
 
 namespace ScoutFieldLog_GroupProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _identityContext;
         private readonly ScoutFieldLogDbContext _context;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public HomeController(ScoutFieldLogDbContext context)
+        public HomeController(ApplicationDbContext identityContext, ScoutFieldLogDbContext context, SignInManager<IdentityUser> signInManager)
 
         {
             _context = context;
+            _identityContext = identityContext;
+            _signInManager = signInManager;
         }
-
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult StartupSearch(string CompanyName)
+        {
+            List<StartUp> startups;
+            if (CompanyName is null || CompanyName == "")
+            {
+               startups = _context.StartUp.ToList();
+            }
+            else
+            {
+               startups = _context.StartUp.Where(x => x.CompanyName.Contains(CompanyName)).ToList();
+            }
+            return View("Index", startups);
         }
 
         public IActionResult Privacy()
