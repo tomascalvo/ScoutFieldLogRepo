11db7f46e3d8f477d86e28e2b8ef7429f3fc98aa
chris <caconstan@yahoo.com>
2020-06-16 13:24:43 -0400

    added textmatch class

---
+++ ScoutFieldLog_GroupProject/Models/StartupMatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScoutFieldLog_GroupProject.Models
+{
+    public class StartupMatch
+    {
+        public static bool Match(string[] args)
+        {
+            //Console.WriteLine("this is a test");
+            return false;
+        }
+    }
+}
